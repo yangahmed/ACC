54ce31435e24e83669e9b6b0c91c474b2734fc6b
yangahmed <yangahmed@gmail.com>
2019-10-02 14:16:55 +0200

    fix

--- A3/count.py
+++ A3/count.py
@@ -1,11 +1,11 @@
 from tasks import count, add
 import os
 
-path = ''
+path = '/home/ubuntu/datadisk/data'
+word = 'han'
 files= os.listdir(path)
 
-count = 0
-word = 'han'
+# count = 0
 for file in files:
     f = open(path+"/"+file)
     result = count.delay(word, f)
