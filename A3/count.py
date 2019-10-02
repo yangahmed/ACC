d3caafd161897014ec943cfbed7893a45507d303
yangahmed <yangahmed@gmail.com>
2019-10-02 21:10:20 +0200

    test

--- A3/count.py
+++ A3/count.py
@@ -1,11 +1,14 @@
 from tasks import count, add
 import os
 
-path = '/home/ubuntu/data'
+# path = '/home/ubuntu/data'
 word = 'han'
-files= os.listdir(path)
+# files= os.listdir(path)
 
 # count = 0
-for file in files:
-    f = open(path+"/"+file)
-    result = count.delay(word, f)
+# for file in files:
+#     f = open(path+"/"+file)
+#     result = count.delay(word, f)
+
+data = open('data/4fe89313-8c7a-4c95-9b3b-155435d25c1a')
+result = count.delay(word, data)
