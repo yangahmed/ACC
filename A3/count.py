4f140aad4aa073f8b4b696cfb44878e5ffc65902
yangahmed <yangahmed@gmail.com>
2019-10-08 22:08:51 +0200

    flask

--- A3/count.py
+++ A3/count.py
@@ -1,17 +1,24 @@
 from tasks import count
 import os
+from flask import Flask
+app = Flask(__name__)
 
 p = '/home/ubuntu/data'
-word = 'han'
-files= os.listdir(p)
+# word = 'han'
 
-result = []
-for file in files:
-    path = p + "/" + file
-    result.append(count.delay(word, path))
+@app.route('/count/<str:word>', methods=['GET'])
+def count_word(word, p):
+    files= os.listdir(p)
+    result = []
+    for file in files:
+        path = p + "/" + file
+        result.append(count.delay(word, path))
 
-c = 0
-for i in range(len(result)):
-    c += result[i].get()
+    c = 0
+    for i in range(len(result)):
+        c += result[i].get()
 
-print(c)
+    print(c)
+
+if __name__ == '__main__':
+    app.run()
