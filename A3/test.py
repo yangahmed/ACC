f1a6a94e88a8c83b70c0e3878a8c7d6b47e99582
yangahmed <yangahmed@gmail.com>
2019-10-09 15:39:47 +0200

    v1

--- A3/test.py
+++ A3/test.py
@@ -1,29 +1,31 @@
-from tasks import count
-import os
 from flask import Flask, jsonify
 from flask import request
-
 app = Flask(__name__)
 
 p = '/home/ubuntu/data'
+# word = 'han'
 
 @app.route('/count', methods=['GET'])
 def count_word():
+    # files= os.listdir(p)
+    # result = []
+    # for file in files:
+    #     path = p + "/" + file
+    #     result.append(count.delay(word, path))
+
+    # c = 0
+    # for i in range(len(result)):
+    #     c += result[i].get()
+
+    # print(c)
     words = request.args.get("words")
     wordlist = words.split(',')
-    files= os.listdir(p)
     res = []
     for word in wordlist:
-        result = []
-        for file in files:
-            path = p + "/" + file
-            result.append(count.delay(word, path))
-
-        c = 0
-        for i in range(len(result)):
-            c += result[i].get()
+        c=1
         res.append({word: c})
     return jsonify(result=res)
+    # return wordlist[0]
 
 if __name__ == '__main__':
     app.run()
